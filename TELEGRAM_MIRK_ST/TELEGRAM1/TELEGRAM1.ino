818ad9d2698bcc62c18a1b1684e005f5a39b767d
uridig <47348001@est.ort.edu.ar>
2024-05-20 18:13:37 -0300

    Update TELEGRAM1.ino

--- TELEGRAM_MIRK_ST/TELEGRAM1/TELEGRAM1.ino
+++ TELEGRAM_MIRK_ST/TELEGRAM1/TELEGRAM1.ino
@@ -27,11 +27,16 @@ const char* password = "OrtIOTnew22$2";
 #define VUELTA_MI 3 //Vuelta modo inicial
 #define AUMENTAR_UMBRAL 4
 
+//CAMBIO, NUEVOS ESTADOS
+#define REDUCIR_UMBRAL 5
+#define MODOINICIAL_PRIMERBOTON 6
+#define MODOINICIAL_SEGUNDOBOTON 7
+
 #define BOTON1 35
 #define BOTON2 34
 
 int estadoActivo = MODOINICIAL;
-int x = 0;
+int y = 0;
 int temp;
 long int tiempo2 = -600000;
 int umbral = 28;
@@ -66,144 +71,171 @@ void setup() {
 }
 
 void loop() {
-  if (dht.readTemperature() >= umbral && millis() - tiempo2 > 6000) {
-    bot.sendMessage(CHAT_ID, "La temp es mayor");
-    tiempo2 = millis();
+  if (dht.readTemperature() >= umbral) {
+    if (y == 0) {
+      bot.sendMessage(CHAT_ID, "La temp es mayor");
+      y = 1;
+    }
+  } else {
+    y = 0;
   }
+  //PRIMER CAMBIO, DA MÁS VELOCIDAD, YA NO SE MANDA EL MENSAJE MUCHAS VECES
 
   maquinaEstado();
 
   if (millis() - tiempomsg > 10000) {
     tiempomsg = millis();
+    funcionTelegram();
+    //SEGUNDO CAMBIO, HACEMOS TELEGRAM DENTRO DE UNA FUNCION
+  }
+
+
+  void maquinaEstado() {
+    Serial.println(estadoActivo);
+
+    switch (estadoActivo) {
+      case MODOINICIAL:
+        if (digitalRead(BOTON1) == 0) {
+          estadoActivo = MODOINICIAL_PRIMERBOTON;
+          tiempo = millis();
+        }
+
+        //CAMBIO, NUEVA FUNCION PARA MOSTRAR TEMP Y UMBRAL
+        mostrarTemp();
+
+        break;
+      case CAMBIAR_UMBRAL:
+
+        u8g2.clearBuffer();  // clear the internal memory
+
+        u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
+
+        u8g2.drawStr(15, 50, "Umbral:");
+        sprintf(stringumbral, "%d", umbral);
+        u8g2.drawStr(70, 50, stringumbral);
+        u8g2.drawStr(90, 50, "C");
+        u8g2.sendBuffer();
+        if (digitalRead(BOTON1) == 0) {
+          estadoActivo = AUMENTAR_UMBRAL;
+        }
+        if (digitalRead(BOTON2) == 0) {
+          estadoActivo = REDUCIR_UMBRAL;
+        }
+        if (digitalRead(BOTON1) == 0 && digitalRead(BOTON2) == 0) {
+          estadoActivo = VUELTA_MI;
+          tiempo = millis();
+        }
+
+
+        break;
+      case PASAJE_UMBRAL:
+        if (digitalRead(BOTON1) == 0 || digitalRead(BOTON2) == 0) {
+          tiempo = millis();
+        }
+        if (millis() - tiempo >= 15) {
+          tiempo = millis();
+          estadoActivo = CAMBIAR_UMBRAL;
+        }
+        break;
+      case VUELTA_MI:
+        if (digitalRead(BOTON1) == 1 && digitalRead(BOTON2) == 1) {
+          estadoActivo = MODOINICIAL;
+          tiempo = millis();
+        }
+        break;
+      case AUMENTAR_UMBRAL:
+        if (digitalRead(BOTON1) == 0 || digitalRead(BOTON2) == 0) {
+          tiempo = millis();
+        }
+        if (digitalRead(BOTON1) == 0 && digitalRead(BOTON2) == 0) {
+          tiempo = millis();
+          estadoActivo = VUELTA_MI;
+        }
+        if (millis() - tiempo >= 15) {
+          umbral++;
+          estadoActivo = CAMBIAR_UMBRAL;
+          tiempo = millis();
+          umbrall = String(umbral);
+        }
+
+        break;
+      //CAMBIO, NUEVO ESTADO
+      case REDUCIR_UMBRAL:
+        if (digitalRead(BOTON1) == 0 || digitalRead(BOTON2) == 0) {
+          tiempo = millis();
+        }
+        if (digitalRead(BOTON1) == 0 && digitalRead(BOTON2) == 0) {
+          tiempo = millis();
+          estadoActivo = VUELTA_MI;
+        }
+        if (millis() - tiempo >= 15) {
+          umbral--;
+          estadoActivo = CAMBIAR_UMBRAL;
+          tiempo = millis();
+          umbrall = String(umbral);
+        }
+
+        break;
+      case MODOINICAL_PRIMERBOTON:
+        if (millis() - tiempo >= 5000) {
+          estadoActivo = MODOINICIAL;
+          tiempo = millis();
+        }
+        if (digitalRead(BOTON2) == 0 && digitalRead(BOTON1) == 1) {
+          estadoActivo = MODOINICIAL_SEGUNDOBOTON;
+          tiempo = millis();
+        }
+        mostrarTemp();
+        break;
+      case MODOINICIAL_SEGUNDOBOTON:
+        if (millis() - tiempo >= 5000) {
+          estadoActivo = MODOINICIAL;
+          tiempo = millis();
+        }
+        if (digitalRead(BOTON1) == 0 && digitalRead(BOTON2) == 1) {
+          estadoActivo = PASAJE_UMBRAL;
+          tiempo = millis();
+        }
+        mostrarTemp();
+        break;
+    }
+  }
+
+  void funcionTelegram() {
     cantMsj = bot.getUpdates(bot.last_message_received + 1);
     if (cantMsj > 0) {
-      String text = bot.messages[0].text;
-      if (text == "/sensor") {
-        String temperaturaString = String(dht.readTemperature());  // Interroga por datos del sensor
-        bot.sendMessage(CHAT_ID, "La temperatura es " + temperaturaString);
-      } else {
-        if (text == "/umbral") {
-          umbrall= String(umbral);
-          bot.sendMessage(CHAT_ID, "El umbral es " + umbrall);
+      for (i = 0, i < cantMsj, i++) {
+        //TERCER CAMBIO, LEEMOS TODOS LOS MENSAJES NUEVOS, NO SOLO EL ULTIMO, USAMOS FOR
+        String text = bot.messages[i].text;
+        if (text == "/sensor") {
+          String temperaturaString = String(dht.readTemperature());  // Interroga por datos del sensor
+          bot.sendMessage(CHAT_ID, "La temperatura es " + temperaturaString);
         } else {
-          bot.sendMessage(CHAT_ID, "Las funciones son /umbral y /sensor");
+          if (text == "/umbral") {
+            umbrall = String(umbral);
+            bot.sendMessage(CHAT_ID, "El umbral es " + umbrall);
+          } else {
+            bot.sendMessage(CHAT_ID, "Las funciones son /umbral y /sensor");
+          }
         }
       }
-
       cantMsj = bot.getUpdates(bot.last_message_received + 1);
     }
   }
-  //bot.sendMessage(CHAT_ID, "Umbral: " + umbrall, "");
 }
+void mostrarTemp() {
+  temp = dht.readTemperature();
+  //Serial.println(temp);
+  u8g2.clearBuffer();  // clear the internal memory
 
-
-void maquinaEstado() {
-  Serial.println(estadoActivo);
-
-  switch (estadoActivo) {
-    case MODOINICIAL:
-      if (x <= 1 && digitalRead(BOTON1) == 0) {
-        x = 1;
-        tiempo = millis();
-      }
-      if (x >= 1 && x <= 2 && digitalRead(BOTON2) == 0 && digitalRead(BOTON1) == 1) {
-        x = 2;
-        tiempo = millis();
-      }
-      if (x == 2 && digitalRead(BOTON1) == 0 && digitalRead(BOTON2) == 1) {
-        estadoActivo = PASAJE_UMBRAL;
-        tiempo = millis();
-        x = 0;
-      }
-      if (millis() - tiempo >= 5000) {
-        x = 0;
-        tiempo = millis();
-      }
-      temp = dht.readTemperature();
-      //Serial.println(temp);
-      u8g2.clearBuffer();  // clear the internal memory
-
-      u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
-      sprintf(stringtemp, "%d", temp);
-      u8g2.drawStr(15, 20, "Temp:");
-      u8g2.drawStr(70, 20, stringtemp);
-      u8g2.drawStr(90, 20, "C");
-      u8g2.drawStr(15, 50, "Umbral:");
-      sprintf(stringumbral, "%d", umbral);
-      u8g2.drawStr(70, 50, stringumbral);
-      u8g2.drawStr(90, 50, "C");
-      u8g2.sendBuffer();
-
-      break;
-    case CAMBIAR_UMBRAL:
-
-      u8g2.clearBuffer();  // clear the internal memory
-
-      u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
-
-      u8g2.drawStr(15, 50, "Umbral:");
-      sprintf(stringumbral, "%d", umbral);
-      u8g2.drawStr(70, 50, stringumbral);
-      u8g2.drawStr(90, 50, "C");
-      u8g2.sendBuffer();
-      if (digitalRead(BOTON1) == 0) {
-        x = 4;
-      }
-      if (digitalRead(BOTON2) == 0) {
-        x = 5;
-      }
-      if (digitalRead(BOTON1) == 0 && digitalRead(BOTON2) == 0) {
-        estadoActivo = VUELTA_MI;
-        tiempo = millis();
-        x = 0;
-      }
-      if (x >= 4) {
-        estadoActivo = AUMENTAR_UMBRAL;
-        tiempo = millis();
-      }
-
-      break;
-    case PASAJE_UMBRAL:
-      if (digitalRead(BOTON1) == 0 || digitalRead(BOTON2) == 0) {
-        tiempo = millis();
-      }
-      if (millis() - tiempo >= 15) {
-        tiempo = millis();
-        estadoActivo = CAMBIAR_UMBRAL;
-      }
-      break;
-    case VUELTA_MI:
-      if (digitalRead(BOTON1) == 1 && digitalRead(BOTON2) == 1) {
-        estadoActivo = MODOINICIAL;
-        tiempo = millis();
-      }
-      break;
-    case AUMENTAR_UMBRAL:
-      if (digitalRead(BOTON1) == 0 || digitalRead(BOTON2) == 0) {
-        tiempo = millis();
-      }
-      if (digitalRead(BOTON1) == 0 && digitalRead(BOTON2) == 0) {
-        tiempo = millis();
-        estadoActivo = VUELTA_MI;
-        x = 0;
-      }
-      if (millis() - tiempo >= 15) {
-        if (x == 4) {
-          umbral++;
-          estadoActivo = CAMBIAR_UMBRAL;
-          x = 0;
-          tiempo = millis();
-          umbrall = String(umbral);
-        }
-        if (x == 5) {
-          umbral--;
-          estadoActivo = CAMBIAR_UMBRAL;
-          x = 0;
-          tiempo = millis();
-          umbrall = String(umbral);
-        }
-      }
-
-      break;
-  }
-} 
+  u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
+  sprintf(stringtemp, "%d", temp);
+  u8g2.drawStr(15, 20, "Temp:");
+  u8g2.drawStr(70, 20, stringtemp);
+  u8g2.drawStr(90, 20, "C");
+  u8g2.drawStr(15, 50, "Umbral:");
+  sprintf(stringumbral, "%d", umbral);
+  u8g2.drawStr(70, 50, stringumbral);
+  u8g2.drawStr(90, 50, "C");
+  u8g2.sendBuffer();
+}
